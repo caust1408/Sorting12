69323fce16fa49de1f2ba09fbbd3ea87a5724e77
Craig <cav9871@saintjoe.edu>
2015-03-20 13:03:23 -0400

    added in the shellsort2

--- printData.js
+++ printData.js
@@ -1,7 +1,18 @@
 load('arrayClass.js');
-var numElements = 100;
+load('shellsort.js');
+var numElements = 10;
 var nums = new Carray(numElements);
 
+nums.setData();
+print('Before: \n');
+print(nums.toString());
+print('\nDuring: \n');
+nums.shellSort();
+print('\nAfter: \n');
+print(nums.toString());
+
+
+/*
 nums.setData();
 var start = new Date().getTime();
 nums.bubbleSort();
@@ -24,5 +35,5 @@ nums.insertionSort();
 var stop = new Date().getTime();
 var elapsed = stop - start;
 print('Elapsed time for the insertion sort on ' + numElements + ' element is: ' + elapsed + ' milliseconds.');
-
+*/
 
