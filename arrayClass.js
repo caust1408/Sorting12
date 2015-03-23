396bb349fc232d5840b0749ee8bd88e6ff118297
Craig <cav9871@saintjoe.edu>
2015-03-23 11:03:22 -0400

    Added in the mergesort function

--- arrayClass.js
+++ arrayClass.js
@@ -17,6 +17,7 @@ function Carray(numElements) {
 	this.shellSort = shellSort;
 	this.gaps = [5,3,1];
 	this.shellSort2 = shellSort2;
+	this.mergeSort = mergeSort;
 
 	for (var i = 0; i < numElements; ++i) {
 		this.dataStore[i] = i;
