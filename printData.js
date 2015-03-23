bf005471c84fe0c4625a9e2153e8e4b3ebf9fce2
Craig <cav9871@saintjoe.edu>
2015-03-23 11:38:17 -0400

    Added the only the times of each algorithm and commented out the actual work of the algorithm

--- printData.js
+++ printData.js
@@ -2,9 +2,10 @@ load('arrayClass.js');
 load('shellsort.js');
 load('shellsort2.js');
 load('mergesort2.js');
-var numElements = 100;
+var numElements = 10000;
 var nums = new Carray(numElements);
 
+/*
 nums.setData();
 print('Before: \n');
 print(nums.toString());
@@ -12,9 +13,10 @@ print('\nDuring: \n');
 nums.mergeSort();
 print('\nAfter: \n');
 print(nums.toString());
+*/
 
 
-/*
+
 nums.setData();
 var start = new Date().getTime();
 nums.bubbleSort();
@@ -37,5 +39,33 @@ nums.insertionSort();
 var stop = new Date().getTime();
 var elapsed = stop - start;
 print('Elapsed time for the insertion sort on ' + numElements + ' element is: ' + elapsed + ' milliseconds.');
-*/
+
+
+
+nums.setData();
+var start = new Date().getTime();
+nums.shellSort();
+var stop = new Date().getTime();
+var elapsed = stop - start;
+print('Elapsed time for the shellSort on ' + numElements + ' element is: ' + elapsed + ' milliseconds.');
+
+
+
+nums.setData();
+var start = new Date().getTime();
+nums.shellSort2();
+var stop = new Date().getTime();
+var elapsed = stop - start;
+print('Elapsed time for the shellSort2 on ' + numElements + ' element is: ' + elapsed + ' milliseconds.');
+
+
+
+nums.setData();
+var start = new Date().getTime();
+nums.mergeSort();
+var stop = new Date().getTime();
+var elapsed = stop - start;
+print('Elapsed time for the merge sort on ' + numElements + ' element is: ' + elapsed + ' milliseconds.');
+
+
 
